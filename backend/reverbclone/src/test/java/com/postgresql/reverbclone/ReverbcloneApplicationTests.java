f6089e07ccfd84afa60bf21f5f3fb6dea0bd4a3e
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-05-07 23:58:39 -0400

    changed springboot project name and metadata, added test controller for springboot

--- backend/reverbclone/src/test/java/com/postgresql/reverbclone/ReverbcloneApplicationTests.java
+++ backend/reverbclone/src/test/java/com/postgresql/reverbclone/ReverbcloneApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.postgresql.reverbclone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ReverbcloneApplicationTests {
 
 	@Test
 	void contextLoads() {
