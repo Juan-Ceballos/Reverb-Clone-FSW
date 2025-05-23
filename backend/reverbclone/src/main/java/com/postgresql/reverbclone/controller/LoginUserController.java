4a7a71fe001865584ff527ceb9700fd0fd9cc785
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-05-23 05:14:05 -0400

    added token to request

--- backend/reverbclone/src/main/java/com/postgresql/reverbclone/controller/LoginUserController.java
+++ backend/reverbclone/src/main/java/com/postgresql/reverbclone/controller/LoginUserController.java
@@ -1,6 +1,7 @@
 package com.postgresql.reverbclone.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,15 +12,12 @@ import com.postgresql.reverbclone.model.Users;
 import com.postgresql.reverbclone.service.UserService;
 
 @RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:5173")
 public class LoginUserController {
 
     @Autowired
     private UserService service;
-
-    @GetMapping("/login")
-    public String showLoginForm() {
-        return "login";
-    }
 
     @PostMapping("/login")
     public String login(@RequestBody Users user) {
