09bc6049bedd047ac66f6c93bb43337da3a36eb6
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-06-16 23:02:06 -0400

    removed unnecessary imports

--- backend/reverbclone/src/main/java/com/postgresql/reverbclone/service/JWTService.java
+++ backend/reverbclone/src/main/java/com/postgresql/reverbclone/service/JWTService.java
@@ -1,15 +1,11 @@
 package com.postgresql.reverbclone.service;
 
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
-
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
