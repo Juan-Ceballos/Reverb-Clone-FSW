223da50f70e5b7e768caaa2b66d132e052c1c25c
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-06-17 01:35:01 -0400

    add skeleton navbar ui component

---
+++ frontend/src/NavBar.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+
+const NavBar = () => {
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default NavBar
