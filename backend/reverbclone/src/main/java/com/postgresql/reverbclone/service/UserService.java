5a736e630740f291c2bde61455499385edc0d15e
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-06-19 06:15:12 -0400

    removed unnecessary imports from backend code

--- backend/reverbclone/src/main/java/com/postgresql/reverbclone/service/UserService.java
+++ backend/reverbclone/src/main/java/com/postgresql/reverbclone/service/UserService.java
@@ -3,7 +3,6 @@ package com.postgresql.reverbclone.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
