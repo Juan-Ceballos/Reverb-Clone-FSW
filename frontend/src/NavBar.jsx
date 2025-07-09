6dcdc2af4a4aeb4cf89e2343029f684c8ece71d3
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-07-08 23:10:28 -0400

    added components together in the navbar component

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import logo from '../src/assets/Reverb-Clone-Logo.svg'
+import ReverbLogoIcon from './Reverb-Clone-Icon'
+import MainSearchBar from './MainSearchBar'
 
 const NavBar = () => {
 
     const navItems = [
-        {name: 'Reverb-Clone', href:'/'},
         {name: 'Search Bar', href:''},
         {name: 'Sign Up', href:'/login'},
         {name: 'Log In', href:'/register'}
@@ -12,9 +13,11 @@ const NavBar = () => {
 
     return (
         <div>
-            <div className='main-navbar'>
-                {navItems.map((item) => {
-                    if(item.name != "Reverb-Clone") {
+            <div className='nav-bar-container'>
+                <ReverbLogoIcon></ReverbLogoIcon>
+                <MainSearchBar></MainSearchBar>
+                <div className='main-navbar'>
+                    {navItems.map((item) => {
                         return(
                             <a 
                                 key={item.name}
@@ -24,25 +27,9 @@ const NavBar = () => {
                                 {item.name}
                             </a>
                         )
-                    } else {
-                        return(
-                            <a
-                                key={item.name}
-                                href={item.href}
-                                className="nav-item-logo"
-                            >
-                                <img 
-                                    id='reverb-clone-logo' 
-                                    src={logo} 
-                                    alt='reverb-clone-logo'
-                                    className='icon'
-                                >
-                                </img>  
-                            </a>
-                        )
-                    }
-            })}
-            </div> 
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
