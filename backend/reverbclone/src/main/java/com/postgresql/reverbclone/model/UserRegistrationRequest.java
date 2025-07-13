f12f8e2d8728a098b1c659ab54022e66fe6815d4
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-07-13 19:14:32 -0400

    added email to user registration request model

--- backend/reverbclone/src/main/java/com/postgresql/reverbclone/model/UserRegistrationRequest.java
+++ backend/reverbclone/src/main/java/com/postgresql/reverbclone/model/UserRegistrationRequest.java
@@ -12,11 +12,15 @@ public class UserRegistrationRequest {
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters")
-
     private String password;
 
-    public UserRegistrationRequest(String username, String password) {
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String email;
+
+    public UserRegistrationRequest(String username, String email, String password) {
         this.username = username;
+        this.email = email;
         this.password = password;
     }
 
@@ -25,4 +29,7 @@ public class UserRegistrationRequest {
 
     public String getPassword() {return password;}
     public void setPassword(String password) {this.password = password;}
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
 }
