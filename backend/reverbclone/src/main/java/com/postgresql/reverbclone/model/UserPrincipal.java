77bcdb1c327069f449f514e83c7a76758228a485
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-07-15 08:08:34 -0400

    user repo fetch and check user by email

--- backend/reverbclone/src/main/java/com/postgresql/reverbclone/model/UserPrincipal.java
+++ backend/reverbclone/src/main/java/com/postgresql/reverbclone/model/UserPrincipal.java
@@ -32,11 +32,6 @@ public class UserPrincipal implements UserDetails {
     }
 
     @Override
-    public String getEmail() {
-        return user.getEmail();
-    }
-
-    @Override
     public boolean isAccountNonExpired() {
         return true;
     }
