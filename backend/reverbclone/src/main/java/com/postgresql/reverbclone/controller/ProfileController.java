35c92978aac48e7829f52b39d40e6eee2efea776
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-07-18 17:56:39 -0400

    add email to profile page using response from backend profilecontroller

--- backend/reverbclone/src/main/java/com/postgresql/reverbclone/controller/ProfileController.java
+++ backend/reverbclone/src/main/java/com/postgresql/reverbclone/controller/ProfileController.java
@@ -44,10 +44,10 @@ public class ProfileController {
             return ResponseEntity.notFound().build();
         }
 
-        // add email to show in profile 
         return ResponseEntity.ok(Map.of(
             "id", user.getId(),
-            "username", user.getUsername()
+            "username", user.getUsername(),
+            "email", user.getEmail()
         ));
     }
 }
