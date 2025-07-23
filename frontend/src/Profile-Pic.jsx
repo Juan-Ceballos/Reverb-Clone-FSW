10ca52f91f3769a6788c85a75b0601ca9523e067
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-07-23 19:09:54 -0400

    added alt to profile-pic

--- frontend/src/Profile-Pic.jsx
+++ frontend/src/Profile-Pic.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const ProfilePic = () => {
     return(
         <div className='profile-pic-container'>
-            <img src='../src/assets/Reverb-Clone-Logo.svg' className='profile-pic'>
-            </img>
+            <img
+                src='../src/assets/Reverb-Clone-Logo.svg'
+                className='profile-pic'
+                alt="Reverb-Clone logo"
+            />
         </div>
     )
 }
