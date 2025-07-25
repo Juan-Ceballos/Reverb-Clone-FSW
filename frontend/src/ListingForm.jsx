67e962ef2efa1a4e88eca9f3fcdc0f5fe89caeda
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-07-25 19:11:28 -0400

    added fetch for servlet controller, method post for add listing form

--- frontend/src/ListingForm.jsx
+++ frontend/src/ListingForm.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 
 const ListingForm = () => {
+    const navigate = useNavigate()
     const [formData, setFormData] = useState({
         itemName: '',
         itemImage: '',
@@ -15,13 +16,42 @@ const ListingForm = () => {
         serialNo: '',
     })
 
+    const [loading, setLoading] = useState(false)
+    const [message, setMessage] = useState('')
+    const [error, setError] = useState('')
+
     const handleChange = (eventObject) => {
         const {name, value} = eventObject.target
         setFormData((prev) => ({...prev, [name]: value}))
     }
 
+    
     const handleSubmit = async(eventObject) => {
+        eventObject.preventDefault()
+        setLoading(true)
+        setMessage('')
+        setError('')
         console.log("Handle Submit")
+        try {
+            const response = await fetch('http://localhost:8080/addlisting', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            if(response.ok) {
+                setMessage('Added Listing!')
+            } else {
+                const errorText = await response.text()
+                setError(`Added Listing Failed: ${response.status} - ${errorText || response.status}`)
+            }
+        } catch(err) {
+            setError(`Network error: ${err.message}`)
+            console.error(`adding listing error:`, err)
+        } finally {
+            setLoading(false)
+        }
     }
     // UI
     return(
