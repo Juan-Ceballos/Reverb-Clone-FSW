4723fa561571b45a2b3477865423ba5fe4cca2e6
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-07-25 17:55:52 -0400

    form handle change and submit

--- frontend/src/ListingForm.jsx
+++ frontend/src/ListingForm.jsx
@@ -2,6 +2,27 @@ import React, {useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 
 const ListingForm = () => {
+    const [formData, setFormData] = useState({
+        itemName: '',
+        itemImage: '',
+        description: '',
+        price: '',
+        category: '',
+        brand: '',
+        finish: '',
+        model: '',
+        year: '',
+        serialNo: '',
+    })
+
+    const handleChange = (eventObject) => {
+        const {name, value} = eventObject.target
+        setFormData((prev) => ({...prev, [name]: value}))
+    }
+
+    const handleSubmit = async(eventObject) => {
+        console.log("Handle Submit")
+    }
     // UI
     return(
         <div>
