04f4d56dbb3c76ec27b8729d5cbd83a799805e72
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-07-30 20:16:57 -0400

    added buttons to modal elements

--- frontend/src/ListingModal.jsx
+++ frontend/src/ListingModal.jsx
@@ -3,6 +3,8 @@ const ListingModal = () => {
         <div className="modalBackground">
             <div className="modalContainer">
                 <h1>Hello How Are You</h1>
+                <button>Save</button>
+                <button>Close</button>
             </div>
         </div>
     )
