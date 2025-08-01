64e5454cc78bf864771401237f796ecdceb182a7
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-08-01 03:38:12 -0400

    change state using prop to remove modal after button press

--- frontend/src/ListingModal.jsx
+++ frontend/src/ListingModal.jsx
@@ -1,10 +1,17 @@
-const ListingModal = () => {
+import React, {useState} from 'react'
+
+const ListingModal = ({ setIsModalOpen }) => {
+
+    const handleClose = () => [
+        setIsModalOpen(false)
+    ]
+
     return(
         <div className="modalBackground">
             <div className="modalContainer">
                 <h1>Hello How Are You</h1>
                 <button>Save</button>
-                <button>Close</button>
+                <button onClick={handleClose}>Close</button>
             </div>
         </div>
     )
