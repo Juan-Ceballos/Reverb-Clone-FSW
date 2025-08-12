64058fbaf9ce39890ff20aa4b88067f79f8897dd
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-08-11 23:19:31 -0400

    same for name item field

--- frontend/src/ListingModal.jsx
+++ frontend/src/ListingModal.jsx
@@ -10,6 +10,10 @@ const ListingModal = ({ setIsModalOpen }) => {
         <div className="modalBackground">
             <div className="modalContainer">
                 <form action="">
+                    <div className='item-name-field'>
+                        <label>Item Name</label>
+                        <input type="text" />
+                    </div>
                     <div className='price-field'>
                         <label for="price-input">Price</label>
                         <input type="text" id='price-input'/>
