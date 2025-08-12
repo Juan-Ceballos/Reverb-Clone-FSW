2ffb5892f0e0251c170b690813584d95be15375a
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-08-11 23:16:37 -0400

    adjusted form inner div for price flex column css

--- frontend/src/ListingModal.jsx
+++ frontend/src/ListingModal.jsx
@@ -10,7 +10,10 @@ const ListingModal = ({ setIsModalOpen }) => {
         <div className="modalBackground">
             <div className="modalContainer">
                 <form action="">
-                    <input type="text" />
+                    <div className='price-field'>
+                        <label for="price-input">Price</label>
+                        <input type="text" id='price-input'/>
+                    </div>  
                 </form>
                 <button>Save</button>
                 <button onClick={handleClose}>Close</button>
